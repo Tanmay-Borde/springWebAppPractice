c19959c865ae00ae0af37791e1f2a74c1d8de474
tanmay.borde <tanmay.borde@veritas.com>
2022-08-11 17:32:06 +0530

    Adding the source code

---
+++ src/main/java/com/practice/springWebAppPractice/SpringWebAppPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.practice.springWebAppPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAppPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebAppPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/springWebAppPractice/SpringWebAppPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.springWebAppPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAppPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
